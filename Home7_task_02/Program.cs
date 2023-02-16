88e096bf003cc342815a44c35f164fcebfb790e0
e.tairov <ev.tairov@yandex.ru>
2023-02-17 03:31:52 +0700

    Home 07_2

--- Home7_task_02/Program.cs
+++ Home7_task_02/Program.cs
@@ -13,3 +13,75 @@
 */
 //------------------------------------------------------
 
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result) && result >= 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не корректное число. Повторите ввод.");
+        }
+    }
+
+    return result;
+}
+int[,] InitMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rnd.Next(1, 30);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+void CheckIndexMatrix(int[,] matrix)
+{
+    Console.WriteLine("Введите индекс i: ");
+    int i = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите индекс j: ");
+    int j = Convert.ToInt32(Console.ReadLine());
+
+    if (i <= matrix.GetLength(0) && j <= matrix.GetLength(1))
+    {
+        //Console.WriteLine("number[{0},{0}] = {1}", j, matrix[i, j]);
+        Console.WriteLine($"Выводим элемент {matrix[i, j]}");
+       
+    }
+
+    else Console.WriteLine($"Нет элемента с введенными индексами");
+}
+
+int countOfRows = GetNumber("Введите кол-во строк:");
+int countOfColumns = GetNumber("Введите кол-во столбцов:");
+int[,] matrix = InitMatrix(countOfRows, countOfColumns);
+
+PrintMatrix(matrix);
+CheckIndexMatrix(matrix);
